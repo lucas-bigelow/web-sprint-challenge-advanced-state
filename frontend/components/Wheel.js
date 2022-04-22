6c4796325e0454d2a774e3117c67139555f7ebcc
Lucas Bigelow <lukejbigelow@gmail.com>
2022-04-22 12:55:40 -0500

    built out all the functionality for persistent state wheel

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default function Wheel(props) {
+import { moveClockwise, moveCounterClockwise } from '../state/action-creators';
+
+function Wheel(props) {
+  const handleClockwise = e => {
+    e.preventDefault();
+    props.moveClockwise();
+  }
+
+  const handleCounterClockwise = e => {
+    e.preventDefault();
+    props.moveCounterClockwise();
+  }
+
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
+        {props.cogs.map(cog => {
+          return <div key={cog.cogNum} className={cog.active ? "cog active" : "cog"} style={{ "--i": cog.cogNum }}>{cog.active && "B"}</div>
+        })}
+        {/* <div className="cog active" style={{ "--i": 0 }}>B</div>
         <div className="cog" style={{ "--i": 1 }}></div>
         <div className="cog" style={{ "--i": 2 }}></div>
         <div className="cog" style={{ "--i": 3 }}></div>
         <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className="cog" style={{ "--i": 5 }}></div>--i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={handleClockwise}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={handleCounterClockwise}>Clockwise</button>
       </div>
     </div>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    cogs: state.wheel.cogs
+  }
+}
+
+export default connect(mapStateToProps, {moveClockwise, moveCounterClockwise})(Wheel);
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,9 +1,75 @@
 // ❗ You don't need to add extra reducers to achieve MVP
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import * as actionTypes from './action-types';
 
-const initialWheelState = 0
+const initialWheelState = {
+  currentActiveIndex: 0,
+  cogs: [
+    { cogNum: 0, active: true },
+    { cogNum: 1, active: false },
+    { cogNum: 2, active: false },
+    { cogNum: 3, active: false },
+    { cogNum: 4, active: false },
+    { cogNum: 5, active: false },
+  ]
+}
 function wheel(state = initialWheelState, action) {
-  return state
+  // first make a new array based on state and a new active index
+  const newCogs = [...state.cogs];
+  let newActiveIndex = state.currentActiveIndex;
+
+  switch(action.type) {
+    case actionTypes.MOVE_CLOCKWISE:
+      // loop through and flip the active cog to be inactive, and activate the new cog with boundary checking, modifying the new array
+      for (let i = 0; i < state.cogs.length; i++) {
+        if (i === state.currentActiveIndex) {
+          // deactivate the current cog, this won't change if we're at the boundary or not
+          newCogs[i] = {...state.cogs[i], active: false}
+
+          // see if we're at the end of the array and loop back to the start if we are
+          if (state.currentActiveIndex === state.cogs.length - 1) {
+            newCogs[0] = {...state.cogs[0], active: true};
+            newActiveIndex = 0;
+          //otherwise, just add one and move along
+          } else {
+            newCogs[i + 1] = {...state.cogs[i + 1], active: true};
+            newActiveIndex = i + 1;
+          }
+        }
+      }
+
+      return {
+        currentActiveIndex: newActiveIndex,
+        cogs: newCogs
+      }
+
+      case actionTypes.MOVE_COUNTERCLOCKWISE:
+        // loop through and flip the active cog to be inactive, and activate the new cog with boundary checking, modifying the new array
+        for (let i = 0; i < state.cogs.length; i++) {
+          if (i === state.currentActiveIndex) {
+            // deactivate the current cog, this won't change if we're at the boundary or not
+            newCogs[i] = {...state.cogs[i], active: false}
+  
+            // see if we're at the beginning of the array and loop back to the end if we are
+            if (state.currentActiveIndex === 0) {
+              newCogs[state.cogs.length - 1] = {...state.cogs[state.cogs.length - 1], active: true};
+              newActiveIndex = state.cogs.length - 1;
+            //otherwise, just subtract one and move along
+            } else {
+              newCogs[i - 1] = {...state.cogs[i - 1], active: true};
+              newActiveIndex = i - 1;
+            }
+          }
+        }
+  
+        return {
+          currentActiveIndex: newActiveIndex,
+          cogs: newCogs
+        }
+
+      default:
+        return state
+  }
 }
 
 const initialQuizState = null
